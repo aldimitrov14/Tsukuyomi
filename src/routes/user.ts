6fea80c285dc152ca687f421304e2644b0c6efec
Alexander Dimitrov <alexander.dimitrov@ibsolution.com>
2024-01-08 12:46:00 +0200

    Add fixes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,17 +3,17 @@ import express, { Request, Response } from 'express'
 const userRouter = express.Router()
 
 userRouter.get('/profile', (req: Request, res: Response) => {
-    if (req.user) {
-      res.send(req.user)
-    } else {
-      res.redirect('/login')
-    }
-  })
-  
+  if (req.user) {
+    res.send(req.user)
+  } else {
+    res.redirect('/login')
+  }
+})
+
 userRouter.get('/logout', (req: Request, res: Response) => {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
-    req.logout(done => { })
-    res.redirect('/')
-  })
+  // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
+  req.logout(done => { })
+  res.redirect('/')
+})
 
 export default userRouter
